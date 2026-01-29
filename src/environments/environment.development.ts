c8a822144acb78ca99a5a627a174a1476135e09b
serrano1996 <rafa.serrano1996@gmail.com>
2026-01-29 16:00:50 +0100

    chore: secure environment variables

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://sdwmlmhddcejjuaxptsg.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkd21sbWhkZGNlamp1YXhwdHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1NTM2MDcsImV4cCI6MjA4NDEyOTYwN30.8IFlV6MkNzYOGdbpQ8EtrnoEn6SptCCriXOAvFQ-_sY'
-};
